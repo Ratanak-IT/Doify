c5c1daec69fe003c576d169b36872793ca76ed78
ratanak <ratanak1intel@gmail.com>
2026-04-19 01:29:37 +0700

    deploy

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -9,39 +9,12 @@ import { NavUser } from "@/components/nav-user";
 import { TeamSwitcher } from "@/components/team-switcher";
 
 import {
-  TerminalSquareIcon,
-  FrameIcon,
-  PieChartIcon,
-  MapIcon,
-} from "lucide-react";
-import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
 } from "./ui/sidebar";
-
-const data = {
-  navMain: [
-    {
-      title: "ISTAD Shop",
-      url: "/dashboard",
-      icon: <TerminalSquareIcon />,
-      isActive: true,
-      items: [
-        { title: "Dashboard", url: "/dashboard" },
-        { title: "Product List", url: "/dashboard/product" },
-        { title: "Settings", url: "#" },
-      ],
-    },
-  ],
-  projects: [
-    { name: "Design Engineering", url: "#", icon: <FrameIcon /> },
-    { name: "Sales & Marketing", url: "#", icon: <PieChartIcon /> },
-    { name: "Travel", url: "#", icon: <MapIcon /> },
-  ],
-};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const userState = useAppSelector((state) => state.auth.user);
@@ -60,11 +33,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <TeamSwitcher />
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        <NavMain />
+        <NavProjects />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={user} />
+        <NavUser/>
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
